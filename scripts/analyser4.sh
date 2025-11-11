c96151bf5229f040bf400deecfc71766970836bc
Shergin ILya <sherginilya@icloud.com>
2025-11-11 14:10:50 +0300

    Create analyser4.sh

---
+++ scripts/analyser4.sh
@@ -0,0 +1,19 @@
+min=100;
+max=-1;
+for i in $(ls $1)
+do
+  for j in $(cat $1$i)
+  do
+    age=$(echo $j | cut -d ":" -f2)
+    if [ $age -lt $min ]
+    then 
+      min=$age
+    if [ $age -gt $max ]
+    fi
+    then
+      max=$age
+    fi
+  done
+done
+echo $min
+echo $max
